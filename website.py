34322436839fd9be7a66eaaad05a01b73326b913
Regulus <regulus.gme@proton.me>
2023-01-17 15:21:17 +0100

    First graphs and basic layout
    Set up layout and graphs
    Added slider and dropdown
    Modified datasets for some operations

--- website.py
+++ website.py
@@ -3,20 +3,14 @@ from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 app = Dash(__name__)
-df = pd.read_csv("ICA2\datasets/vacDataClean.csv")
-
+vacData = pd.read_csv("ICA2\datasets/vacDataClean.csv")
+vacTotal = pd.read_csv("ICA2\datasets/vacTotalData.csv")
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
-fig = px.bar(df, x= 'YearWeek', y=['FirstDose','SecondDose'],barmode='group')
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
 
 app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
     html.H1(
@@ -27,14 +21,56 @@ app.layout = html.Div(style={'backgroundColor': colors['background']},children=[
         }
         ),
     
-    html.Div(children='''
-        C19 data
-    '''),
+    html.Label('Choose Graph'),
+    dcc.Dropdown(['Total Vaccine Stats', 'Vaccination by Countries', 'Transmission'], 'Total Vaccine Stats',id='graph_selector'),
+    html.Br(),
     
     dcc.Graph(
-        id = 'graph1',
-        figure = fig,
+        id = 'graph',
+    ),
+    dcc.Slider(
+        vacData.YearWeek[0],
+        vacData.YearWeek[201949],
+        step = None,
+        id = 'yearWeekSlider',
+        value = vacData.YearWeek[201949],
+        marks = {str(yearWeek): str(yearWeek) for yearWeek in vacData.YearWeek.unique()}
     )
 ])
+@app.callback(
+    Output('graph','figure'),
+    Input('graph_selector','value')
+)
+def update_figure(graph_selector):
+    if graph_selector == 'Total Vaccine Stats':
+        fig = vacTotalFigure = go.Figure(
+        data=[
+            go.Bar(x = vacTotal.Vaccine, y = vacTotal.FirstTotal,name='First Dose'),
+            go.Bar(x = vacTotal.Vaccine, y = vacTotal.SecondTotal,name='Second Dose')
+            ],
+        layout=dict(title=dict(text='Total of people vaccinated with such Vaccine'))
+        )
+        vacTotalFigure.update_layout(
+            plot_bgcolor=colors['background'],
+            paper_bgcolor=colors['background'],
+            font_color=colors['text'],
+            title_xref='container',
+        )
+    elif graph_selector == 'Vaccination by Countries':
+        fig = vacCountryFigure = go.Figure(
+            data=[
+                go.Bar(x=vacData.YearWeek, y= vacData.FirstDose,hovertext=vacData.Country,name='First Dose'),
+                go.Bar(x=vacData.YearWeek, y= vacData.SecondDose,hovertext=vacData.Country,name='Second Dose')
+            ],
+            layout=dict(title=dict(text='Vaccination each week in different countries'))
+        )
+        vacCountryFigure.update_layout(
+            plot_bgcolor=colors['background'],
+            paper_bgcolor=colors['background'],
+            font_color=colors['text'],
+            title_xref='container',
+        )
+    
+    return fig
 if __name__ == '__main__':
     app.run_server(debug=True)
